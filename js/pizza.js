e5fa59d72410e1ad93b14f66470eeed914f76f75
Kaan Ertugrul <kaan.hh@live.de>
2024-12-09 00:53:48 +0100

    Navbar und Grundgerüst fertig

---
+++ js/pizza.js
@@ -0,0 +1,6 @@
+import { initPizzaConfigurator } from "./js/pizza.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    initPizzaConfigurator();
+    console.log("Alle Module initialisiert!");
+});
